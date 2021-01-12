8da84eda7f76c50da9d12bf1bdd6f144f7377569
squash1990 <jose.ignacio.cas@gmail.com>
2021-01-12 11:42:00 +0000

    Implementacion del código para conectar con Cloud-SQL y cambio de variables locales a variables de entorno

--- catalogo/app/db/database.py
+++ catalogo/app/db/database.py
@@ -1,14 +1,36 @@
 import psycopg2
 from flask import g
+import os
 
 def get_db():
-    if 'db' not in g:
+
+    db_user = os.environ.get('DB_USERNAME')
+    db_password = os.environ.get('DB_PASSWORD')
+    db_name = os.environ.get('DB_NAME')
+    db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME', None)
+
+    if 'db' not in g and not db_connection_name:
+		# If there is no db_connection_name we assume that we are connecting 
+		# using a TCP socket, and therefore we need a HOST and PORT to 
+		# connect to. 
+        db_host = os.environ.get('DB_HOST')
+        db_port = os.environ.get('DB_PORT')
         g.db = psycopg2.connect(
-            user="dbuser",
-            password="secreto",
-            host="db",
-            port="5432",
-            database="db"
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+            database=db_name
+        )
+    if 'db' not in g and db_connection_name:
+        # If there is a db_connection_name then we assumen that we are
+		# connection to a unix socket, whose name is google-scpecific.
+        unix_socket = '/cloudsql/{}'.format(db_connection_name)
+        g.db = psycopg2.connect(
+            user=db_user,
+            password=db_password,
+            host=unix_socket,
+            database=db_name
         )
     return g.db
 
